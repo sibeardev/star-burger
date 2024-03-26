98fcaa768f5b8bd3167085a81356caf7ff70a347
dad-siberian <dad.siberian@gmail.com>
2024-03-26 17:04:50 +0300

    Add models for order data

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -1,26 +1,24 @@
-from django.db import models
 from django.core.validators import MinValueValidator
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Restaurant(models.Model):
-    name = models.CharField(
-        'название',
-        max_length=50
-    )
+    name = models.CharField("название", max_length=50)
     address = models.CharField(
-        'адрес',
+        "адрес",
         max_length=100,
         blank=True,
     )
     contact_phone = models.CharField(
-        'контактный телефон',
+        "контактный телефон",
         max_length=50,
         blank=True,
     )
 
     class Meta:
-        verbose_name = 'ресторан'
-        verbose_name_plural = 'рестораны'
+        verbose_name = "ресторан"
+        verbose_name_plural = "рестораны"
 
     def __str__(self):
         return self.name
@@ -28,57 +26,47 @@ class Restaurant(models.Model):
 
 class ProductQuerySet(models.QuerySet):
     def available(self):
-        products = (
-            RestaurantMenuItem.objects
-            .filter(availability=True)
-            .values_list('product')
-        )
+        products = RestaurantMenuItem.objects.filter(
+            availability=True
+        ).values_list("product")
         return self.filter(pk__in=products)
 
 
 class ProductCategory(models.Model):
-    name = models.CharField(
-        'название',
-        max_length=50
-    )
+    name = models.CharField("название", max_length=50)
 
     class Meta:
-        verbose_name = 'категория'
-        verbose_name_plural = 'категории'
+        verbose_name = "категория"
+        verbose_name_plural = "категории"
 
     def __str__(self):
         return self.name
 
 
 class Product(models.Model):
-    name = models.CharField(
-        'название',
-        max_length=50
-    )
+    name = models.CharField("название", max_length=50)
     category = models.ForeignKey(
         ProductCategory,
-        verbose_name='категория',
-        related_name='products',
+        verbose_name="категория",
+        related_name="products",
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
     )
     price = models.DecimalField(
-        'цена',
+        "цена",
         max_digits=8,
         decimal_places=2,
-        validators=[MinValueValidator(0)]
+        validators=[MinValueValidator(0)],
     )
-    image = models.ImageField(
-        'картинка'
-    )
+    image = models.ImageField("картинка")
     special_status = models.BooleanField(
-        'спец.предложение',
+        "спец.предложение",
         default=False,
         db_index=True,
     )
     description = models.TextField(
-        'описание',
+        "описание",
         max_length=200,
         blank=True,
     )
@@ -86,8 +74,8 @@ class Product(models.Model):
     objects = ProductQuerySet.as_manager()
 
     class Meta:
-        verbose_name = 'товар'
-        verbose_name_plural = 'товары'
+        verbose_name = "товар"
+        verbose_name_plural = "товары"
 
     def __str__(self):
         return self.name
@@ -96,28 +84,87 @@ class Product(models.Model):
 class RestaurantMenuItem(models.Model):
     restaurant = models.ForeignKey(
         Restaurant,
-        related_name='menu_items',
+        related_name="menu_items",
         verbose_name="ресторан",
         on_delete=models.CASCADE,
     )
     product = models.ForeignKey(
         Product,
         on_delete=models.CASCADE,
-        related_name='menu_items',
-        verbose_name='продукт',
+        related_name="menu_items",
+        verbose_name="продукт",
     )
     availability = models.BooleanField(
-        'в продаже',
-        default=True,
-        db_index=True
+        "в продаже", default=True, db_index=True
     )
 
     class Meta:
-        verbose_name = 'пункт меню ресторана'
-        verbose_name_plural = 'пункты меню ресторана'
-        unique_together = [
-            ['restaurant', 'product']
-        ]
+        verbose_name = "пункт меню ресторана"
+        verbose_name_plural = "пункты меню ресторана"
+        unique_together = [["restaurant", "product"]]
 
     def __str__(self):
         return f"{self.restaurant.name} - {self.product.name}"
+
+
+class Order(models.Model):
+    firstname = models.CharField(
+        "Имя",
+        max_length=50,
+        db_index=True,
+    )
+    lastname = models.CharField(
+        "Фамилия",
+        max_length=50,
+        db_index=True,
+    )
+    address = models.CharField(
+        "Адрес",
+        max_length=150,
+        db_index=True,
+    )
+    phonenumber = PhoneNumberField(
+        "Телефон",
+        region="RU",
+        db_index=True,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Создано",
+    )
+
+    class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
+
+    def __str__(self):
+        return f"Заказ № {self.pk}"
+
+
+class OrderItem(models.Model):
+    """Связывающая модель, которая отображает связь между заказом и продуктом,
+    и количество каждого продукта в заказе"""
+
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name="items",
+        verbose_name="Заказ",
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name="items",
+        verbose_name="Товар",
+    )
+    quantity = models.PositiveIntegerField(
+        default=1,
+        verbose_name="Количество",
+    )
+
+    class Meta:
+        verbose_name = "Товар в заказе"
+        verbose_name_plural = "Товары в заказе"
+
+    def __str__(self):
+        return f"{self.product.name} для заказа №{self.order.pk}"
--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,62 +1,103 @@
-from django.http import JsonResponse
+import json
+
+from django.http import HttpRequest, JsonResponse
 from django.templatetags.static import static
 
-
-from .models import Product
+from .models import Order, OrderItem, Product
 
 
 def banners_list_api(request):
     # FIXME move data to db?
-    return JsonResponse([
-        {
-            'title': 'Burger',
-            'src': static('burger.jpg'),
-            'text': 'Tasty Burger at your door step',
+    return JsonResponse(
+        [
+            {
+                "title": "Burger",
+                "src": static("burger.jpg"),
+                "text": "Tasty Burger at your door step",
+            },
+            {
+                "title": "Spices",
+                "src": static("food.jpg"),
+                "text": "All Cuisines",
+            },
+            {
+                "title": "New York",
+                "src": static("tasty.jpg"),
+                "text": "Food is incomplete without a tasty dessert",
+            },
+        ],
+        safe=False,
+        json_dumps_params={
+            "ensure_ascii": False,
+            "indent": 4,
         },
-        {
-            'title': 'Spices',
-            'src': static('food.jpg'),
-            'text': 'All Cuisines',
-        },
-        {
-            'title': 'New York',
-            'src': static('tasty.jpg'),
-            'text': 'Food is incomplete without a tasty dessert',
-        }
-    ], safe=False, json_dumps_params={
-        'ensure_ascii': False,
-        'indent': 4,
-    })
+    )
 
 
 def product_list_api(request):
-    products = Product.objects.select_related('category').available()
+    products = Product.objects.select_related("category").available()
 
     dumped_products = []
     for product in products:
         dumped_product = {
-            'id': product.id,
-            'name': product.name,
-            'price': product.price,
-            'special_status': product.special_status,
-            'description': product.description,
-            'category': {
-                'id': product.category.id,
-                'name': product.category.name,
-            } if product.category else None,
-            'image': product.image.url,
-            'restaurant': {
-                'id': product.id,
-                'name': product.name,
-            }
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+            "special_status": product.special_status,
+            "description": product.description,
+            "category": (
+                {
+                    "id": product.category.id,
+                    "name": product.category.name,
+                }
+                if product.category
+                else None
+            ),
+            "image": product.image.url,
+            "restaurant": {
+                "id": product.id,
+                "name": product.name,
+            },
         }
         dumped_products.append(dumped_product)
-    return JsonResponse(dumped_products, safe=False, json_dumps_params={
-        'ensure_ascii': False,
-        'indent': 4,
-    })
+    return JsonResponse(
+        dumped_products,
+        safe=False,
+        json_dumps_params={
+            "ensure_ascii": False,
+            "indent": 4,
+        },
+    )
 
 
-def register_order(request):
-    # TODO это лишь заглушка
-    return JsonResponse({})
+def register_order(request: HttpRequest):
+    try:
+        payload = json.loads(request.body)
+
+        order = Order.objects.create(
+            firstname=payload.get("firstname"),
+            lastname=payload.get("lastname"),
+            phonenumber=payload.get("phonenumber"),
+            address=payload.get("address"),
+        )
+        order_items = [
+            OrderItem(
+                order=order,
+                product_id=product.get("product"),
+                quantity=product.get("quantity"),
+            )
+            for product in payload.get("products")
+        ]
+
+        OrderItem.objects.bulk_create(order_items)
+
+        return JsonResponse(
+            {"success": "Order created successfully"}, status=201
+        )
+
+    except json.JSONDecodeError:
+        return JsonResponse({"error": "Invalid JSON format"}, status=400)
+    except Product.DoesNotExist:
+        return JsonResponse({"error": "Product not found"}, status=404)
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
