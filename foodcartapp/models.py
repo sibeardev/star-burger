3c0728a00c424f95bb5d9d831291c19116644a5c
dad-siberian <dad.siberian@gmail.com>
2024-03-31 08:22:43 +0300

    added a comment to the order model

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -153,6 +153,7 @@ class Order(models.Model):
         default=OrderStatus.NEW,
         db_index=True,
     )
+    comment = models.TextField("Комментарий", blank=True)
 
     objects = OrderQuerySet.as_manager()
 
